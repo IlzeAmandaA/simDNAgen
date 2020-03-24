437ff4ebda4a783abda59f914c3382d91047ea38
IlzeAmandaA <ilze.amanda.auzina@gmail.com>
2020-03-24 14:03:39 +0100

    code for generating simulation dna

---
+++ generate.py
@@ -0,0 +1,71 @@
+import random
+import numpy as np
+from scipy.stats import poisson
+from motif_probabilities import Motifs
+
+#20 genes
+# ACGT = 0.25 probability
+#create 20 reference genes
+keys = ['a', 'c', 'g', 't']
+probabilties = [0.25, 0.25, 0.25, 0.25]
+people_count=157
+output_dict = 'output/'
+
+motifs = Motifs('motifs')
+# print(motifs.motifs)
+
+
+genes={}
+lenghts = [25370, 47402, 84651, 33734, 30160,
+           2980, 18541, 98820, 32188,
+           3353, 5415, 3736, 3180, 7038,
+           21693, 3065, 3108, 4176]
+
+
+for idx,n in enumerate(lenghts):
+    print('Gene selected ', idx)
+    gene=list(''.join(np.random.choice(keys, n, replace=True, p=probabilties)))
+    if idx>=9:
+        #generate random mutations
+        for person in range(people_count):
+            for mutatation in range(int(len(gene)*0.01)):
+                m=random.randint(0, len(gene)-1)
+                gene[m]=np.random.choice(keys, 1, replace=True, p=probabilties)[0]
+
+
+            f=open(output_dict+'person'+str(person)+'.txt', 'a')
+            f.write('chrom'+str(idx)+','+''.join(gene)+','+str(0)+'\n')
+            f.close()
+
+
+    else:
+        #insert motifs based on poisson distribution
+        num_average = int((n * 0.055) / 11)
+        num_poisson = poisson(num_average, loc=int(n / 3000)).rvs()
+        # print(num_average, num_poisson)
+        gene_orig=gene.copy()
+
+        for person in range(people_count):
+            gene = gene_orig.copy()
+            used_idx=[]
+            for insertion in range(num_poisson): #number of insterstions needed
+                m = random.randint(0, len(gene)-12) #random location
+                while m in used_idx:
+                    m = random.randint(0, len(gene)-12)
+
+                if idx<3:
+                    ppm_motif = motifs.motifs['NRF1']
+                elif idx>=3 and idx<6:
+                    ppm_motif = motifs.motifs['TFAP2A']
+                else:
+                    motif= ['NRF1','TFAP2A'][random.randint(0,1)]
+                    ppm_motif=motifs.motifs[motif]
+
+                for i_n,n in enumerate(range(len(ppm_motif))): #selected motif length
+                    letter = np.random.choice(keys, 1, replace=True, p=ppm_motif[i_n])[0]
+                    gene[m+i_n] = letter
+                    used_idx.append(m+i_n)
+
+            f = open(output_dict+'person' + str(person) + '.txt', 'a')
+            f.write('chrom' + str(idx) + ',' + ''.join(gene) + ',' + str(1) + '\n')
+            f.close()
---
+++ motif_probabilities.py
@@ -0,0 +1,52 @@
+
+import os
+import torch
+
+class Motifs():
+
+    def __init__(self, filepath):
+        self.filenames = self.obtain_all_files(filepath)
+        self.motifs = {}
+        self.motif_generation()
+
+        #load 10 motifs of interest
+    def obtain_all_files(self,filepath):
+        """
+            Returns all files for a specific folder that end in .txt
+        """
+        file_list = []
+        for filename in os.listdir(filepath):
+            if filename.endswith(".pfm"):
+                file_list.append(os.path.join(filepath, filename))
+                continue
+            else:
+                continue
+        return file_list
+
+    def motif_generation(self):
+        for file in self.filenames:
+            f = open(file)
+            P_counts = []
+            for i,line in enumerate(f):
+                if i==0:
+                    name = line.split(' ')[1]
+                    name=name[:-1]
+                    # print(name)
+                else:
+                    counts = []
+                    for i in line.split(' '):
+                        try:
+                            counts.append(float(i))
+                        except ValueError:
+                            pass
+                    P_counts.append(counts)
+            P_counts = torch.tensor(P_counts, dtype=torch.float32)
+            sum = P_counts.sum(dim=0)
+            PPM = P_counts/sum
+            # PPW = torch.log2((PPM/0.25))
+            # PPW = PPW.permute(1,0) #convert to seq_idx X letter_prob
+            self.motifs[name]=PPM.permute(1,0).numpy()
+
+
+
+
